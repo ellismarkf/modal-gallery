7d2d885598aa9265ac1a3ea0dcfc9e1086e82afc
Mark Ellis <ellis.mark.f@gmail.com>
2014-10-07 19:27:14 -0500

    working content switcher for pop-up window interface

--- js/m-switch.js
+++ js/m-switch.js
@@ -1,6 +1,14 @@
-var galleryPreviews = document.getElementsByClassName('gallery-previews')[0];
+(function(){
+	var galleryPreviews = document.getElementsByClassName('gallery-previews')[0];
+	var galleryFocusImg = document.getElementsByClassName('gallery-focus')[0].children[0];
 
-galleryPreviews.addEventListener('click', function(event) {
-	event.preventDefault();
-
-})
+	galleryPreviews.addEventListener('click', function(event) {
+		event.preventDefault();
+		if (event.target.tagName == "IMG") {
+			var previewImg = event.target;
+			var newSrc = previewImg.attributes[0].value;
+			galleryFocusImg.attributes[0].value = newSrc;
+		}
+		else return;
+	})
+})();
